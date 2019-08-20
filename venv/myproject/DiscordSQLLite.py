ce822f1a3507597568b76b468a617507a1ddf556
Matt <matthewzegilla@gmail.com>
2019-08-20 01:20:07 -0400

    Cleaned Code. End of Day Commit. Stable.

--- venv/myproject/DiscordSQLLite.py
+++ venv/myproject/DiscordSQLLite.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
-# from DiscordBotTest import pass_discord_id
 
 BASE = declarative_base()
 SQL_LITE_DB_NAME = 'Dischord' + '.db'
@@ -9,7 +8,6 @@ SQL_LITE_ENGINE_URL = 'sqlite:///' + SQL_LITE_DB_NAME
 
 
 def get_engine():
-    """Returns SQL engine depending on OS"""
     engine_url = SQL_LITE_ENGINE_URL
     engine = create_engine(engine_url)
     return engine
@@ -30,7 +28,6 @@ class User(BASE):
 
 
 def find_discord_id_balance(discord_id_in):
-    # active_discord_user = pass_discord_id()
     session = get_session()
     users = session.query(User).all()
     for i in users:
@@ -44,13 +41,11 @@ def find_user_exists(discord_id_in):
     users = session.query(User).all()
     for i in users:
         if i.discord_id == discord_id_in:
-            print("true")
             return True
             break
 
-
     session.close()
-    # active_discord_user = "Matt#2929"
+
 
 
 def adduser(discord_id_in):
